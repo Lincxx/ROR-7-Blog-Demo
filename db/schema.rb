be086d87aa388e9843b54af09752847587370323
Lincxx <bossco28@gmail.com>
2022-05-10 14:34:14 -0400

    Added more styling and views counter

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_09_202642) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_10_151804) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
