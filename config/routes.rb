ba340ad61cfc1af6e5876335ca5f136a188ad339
Lincxx <bossco28@gmail.com>
2022-05-10 16:24:42 -0400

    Setup Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
   root "pages#home"
   # get 'pages/home'
   # get 'pages/about'
   #org ^^ 
   get 'about', to: 'pages#about'
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
